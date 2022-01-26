807ed245cc327aab2d111b56035e3d6fdd9facf0
Alex Spiers <advspiers@gmail.com>
2022-01-26 13:33:02 +0000

    Complete auto model monitoring

--- workspace/fullprocess.py
+++ workspace/fullprocess.py
@@ -1,9 +1,9 @@
-import training
-import scoring
+from training import train_model
+from scoring import score_model
 from ingestion import merge_multiple_dataframe as ingest
-import deployment
-import diagnostics
-import reporting
+from deployment import store_model_into_pickle as deploy
+from reporting import score_model as cfm_report
+from apicalls import run_api_call
 import json
 import os
 import ast
@@ -17,6 +17,8 @@ features = ast.literal_eval(config['feature_names'])
 output_folder_path = config['output_folder_path']
 ingested_data_path = os.path.join(os.getcwd(), output_folder_path, 'finaldata.csv')
 model_dir = os.path.join(os.getcwd(), config['output_model_path'])
+clf_save_path = os.path.join(os.getcwd(), model_dir, 'trainedmodel.pkl')
+test_data_path = os.path.join(config['test_data_path'], 'testdata.csv') 
 
 ##################Check and read new data
 def check_new_data(deployment_dir, source_data_dir):
@@ -28,7 +30,7 @@ def check_new_data(deployment_dir, source_data_dir):
 
     new_files = set(input_files) - set(ingested_files)
     is_new_data = True if len(new_files) >= 1 else False
-    return (is_new_data, list(new_files))
+    return is_new_data, list(new_files)
 
 ##################Deciding whether to proceed, part 1
 #if you found new data, you should proceed. otherwise, do end the process here
@@ -36,38 +38,63 @@ def check_new_data(deployment_dir, source_data_dir):
 
 ##################Checking for model drift
 #check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
-latest_score_path = os.path.join(os.getcwd(), deployment_dir, 'latestscore.txt')
-with open(latest_score_path, 'r') as f:
-    current_score = float(f.read())
+def check_for_model_drift(deployment_dir, ingested_data_path, features):
+    latest_score_path = os.path.join(os.getcwd(), deployment_dir, 'latestscore.txt')
+    with open(latest_score_path, 'r') as f:
+        current_score = float(f.read())
 
-# Evaluate F1 score based on new data
-new_score = scoring.score_model(model_dir, ingested_data_path, features)
-print(current_score)
-print(new_score)
+    # Evaluate F1 score based on new data
+    new_score = score_model(deployment_dir, ingested_data_path, features, save = False)
+    is_drift = True if new_score < current_score else False
+    return is_drift, current_score, new_score
 
 ##################Deciding whether to proceed, part 2
 #if you found model drift, you should proceed. otherwise, do end the process here
 
-
-
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
+##################Training and Re-deployment
+#if you found evidence for model drift, score model on test data and save score, and then re-run the deployment.py script
+# NOT APPROPRIATE TO TEST MODEL ON SAME DATA - ISSUE WITH INSTRUCTIONS - WHAT TO TEST DATA ON PREVENT training set leakage?
 
 ##################Diagnostics and reporting
 #run diagnostics.py and reporting.py for the re-deployed model
 
-def run_full_pipeline(deployment_dir, source_data_dir):
+def run_full_pipeline(deployment_dir, source_data_dir, ingested_data_path, model_dir, clf_save_path, features):
+    # Check for new data
+    print("Checking for new data...")
     is_new_data, new_files = check_new_data(deployment_dir, source_data_dir)
+
+    # If no new data - stop process
     if is_new_data is False:
-        return "No new data"
+        return "No new data found - process stopped"
+
+    # Execute data ingestion
+    print("New data found - running data ingestion")
     new_df = ingest(source_data_dir, output_folder_path)
-    return new_df
 
-
+    # Check for model drift - stop process if model drift not occurred
+    print("Checking for model drift...")
+    is_drift, current_score, new_score = check_for_model_drift(deployment_dir, ingested_data_path, features)
+    if is_drift is False:
+        return "No model drift - process stopped"
+    
+    # Retrain, save new score deploy model with ingestion and score records
+    print("Model drift found - retraining ML model on new data...")
+    train_model(ingested_data_path, clf_save_path)
+    new_score = score_model(model_dir, ingested_data_path, features, save = True)
+    print("New F1 score of {} is based on model trained data from {} folder".format(new_score, output_folder_path))
+    deploy(model_dir, output_folder_path, deployment_dir)
+    
+    # Running diagnostics
+    deployed_model_path = os.path.join(os.getcwd(), deployment_dir, 'trainedmodel.pkl')
+    cfm_report(deployed_model_path, ingested_data_path, features)
+    run_api_call(data_path = 'ingesteddata/finaldata.csv')
+    return "Retraining, deployment and reporting completed"
 
 if __name__ == '__main__':
     pass
-    # print(run_full_pipeline(deployment_dir, source_data_dir))
+    result = run_full_pipeline(deployment_dir, source_data_dir, ingested_data_path, model_dir, clf_save_path, features)
+    print(result)
+    
 
 
 
